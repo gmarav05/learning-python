e1e41e2af3ba7ce3e3ffe39012eb4f27442402c7
Aravind <gmarav005@gmail.com>
2025-12-28 16:14:21 +0530

    Add files from List folder

---
+++ List/addlist.py
@@ -0,0 +1,5 @@
+thislist = ["apple", "banana", "cherry"]
+
+thislist.append("watermelon")
+
+print(thislist)
---
+++ List/modifylist.py
@@ -0,0 +1,8 @@
+thislist = ["apple", "banana", "cherry"]
+
+thislist[1:3] = ["watermelon"]
+
+thislist[2:3] = ["totota"]
+thislist.insert(4,"Tata")
+
+print(thislist)
